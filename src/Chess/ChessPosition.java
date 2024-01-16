b7d4ee777e05b5813749cfc7d2c40e36a8c92265
ArturHSousa <artuzeira100@gmail.com>
2024-01-16 18:51:54 -0300

    BUG resolvido

--- src/Chess/ChessPosition.java
+++ src/Chess/ChessPosition.java
@@ -26,17 +26,17 @@ public class ChessPosition {
     public char getColumn() {
         return column;
     }
-
+    
     public int getRow() {
         return row;
     }
-
+    
     protected Position toPosition(){
         return new Position(8 - row, column - 'a');
     }
-    
+       
     protected static ChessPosition fromPosition(Position position){
-        return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+        return new ChessPosition((char)('a' + position.getColumn()), 8 - position.getRow());
     }
     @Override
     public String toString(){

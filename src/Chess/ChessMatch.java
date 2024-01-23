65032472a815e54fd9ff0676e8debde9c70de2a1
ArturHSousa <artuzeira100@gmail.com>
2024-01-23 14:55:00 -0300

    Turno do Jogador

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -16,13 +16,25 @@ import boardgame.Position;
  */
 public class ChessMatch {
     
+    private int turn;
+    private Color currentPlayer;
     private Board board;
 	
 	public ChessMatch() {
 		board = new Board(8, 8);
+                turn = 1;
+		currentPlayer = Color.WHITE;
 		initialSetup();
 	}
 	
+        public int getTurn() {
+            return turn;
+	}
+
+	public Color getCurrentPlayer() {
+            return currentPlayer;
+	}
+
 	public ChessPiece[][] getPieces() {
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
 		for (int i=0; i<board.getRows(); i++) {
@@ -45,6 +57,7 @@ public class ChessMatch {
 		validateSoucePosition(source);
 		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
+                nextTurn();
 		return (ChessPiece)capturedPiece;
 	}
 	
@@ -59,6 +72,9 @@ public class ChessMatch {
        if (!board.thereIsAPiece(position)){
            throw new ChessException("Não há nenhuma peça na posição de origem");
        }
+       	if (currentPlayer != ((ChessPiece)board.piece(position)).getColor()) {
+            throw new ChessException("Essa peça não é sua!");
+	}
        if (!board.piece(position).isThereAnyPossibleMove()) {
            throw new ChessException("Não há movimentos possiveis para a peça escolhida");
        }
@@ -70,6 +86,11 @@ public class ChessMatch {
        }
    }
    
+   private void nextTurn() {
+	turn++;
+	currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
+    }
+
    private void placeNewPiece(char column, int row, ChessPiece piece){
        board.placePiece(piece,new ChessPosition(column, row).toPosition());
    }
--- src/application/UI.java
+++ src/application/UI.java
@@ -4,6 +4,7 @@
  */
 package application;
 
+import Chess.ChessMatch;
 import Chess.ChessPiece;
 import Chess.ChessPosition;
 import Chess.Color;
@@ -59,6 +60,13 @@ public class UI {
         }       
     }
         
+      	public static void printMatch(ChessMatch chessMatch) {
+		printBoard(chessMatch.getPieces());
+		System.out.println();
+		System.out.println("Turno : " + chessMatch.getTurn());
+		System.out.println("Aguardando Jogador: " + chessMatch.getCurrentPlayer());
+	}
+
     public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
